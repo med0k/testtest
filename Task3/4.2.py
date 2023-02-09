b41790819719ffd126c21f57373853f9bb4617bd
med0k <zvoleikom@gmail.com>
2023-02-09 11:58:59 +0200

    commit Homework3 task4.2

---
+++ Task3/4.2.py
@@ -0,0 +1,7 @@
+"""Преобразовать строку в переменной mac из формата XXXX:XXXX:XXXX в формат XXXX.XXXX.XXXX Полученную новую строку
+вывести на стандартный поток вывода (stdout) с помощью print."""
+
+mac = "AAAA:BBBB:CCCC"
+new_string = mac.replace(":", ".")
+
+print(new_string)
