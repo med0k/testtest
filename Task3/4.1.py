7d0a593e24b6dc033ee3467c70be920477dca794
med0k <zvoleikom@gmail.com>
2023-02-09 12:48:14 +0200

    commit Homework3 task4.3

--- Task3/4.1.py
+++ Task3/4.1.py
@@ -1,5 +1,6 @@
 """Используя подготовленную строку nat, получить новую строку, в которой в имени интерфейса вместо FastEthernet
-написано GigabitEthernet. Полученную новую строку вывести на стандартный поток вывода (stdout) с помощью print."""
+написано GigabitEthernet. Полученную новую строку вывести на стандартный поток вывода (stdout) с помощью print.
+"""
 
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
 
