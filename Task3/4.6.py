3bb7194c0e41b9b2fdd467d3437167ec9487fa08
med0k <zvoleikom@gmail.com>
2023-02-12 19:40:44 +0200

    commit Homework3 task4.6

--- Task3/4.6.py
+++ Task3/4.6.py
@@ -4,8 +4,8 @@
 
 ospf_route = "       10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 
-spli_st = ospf_route.replace("via", "").split()
+spli_st = ospf_route.replace("via", "").replace("[", "").replace("]", "").replace(",", "").split()
 
 print(spli_st)
-print("Prefix: ", spli_st[0], "\nAD/Metric: ", spli_st[1], "\nNext-Hop: ", spli_st[2],
+print("Prefix: ",  spli_st[0], "\nAD/Metric: ", spli_st[1], "\nNext-Hop: ", spli_st[2],
       "\nLast update: ", spli_st[3], "\nOutbound Interface: ", spli_st[2])
