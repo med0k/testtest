d8a73955bfa835600f3440ea8166dca76245bebe
med0k <zvoleikom@gmail.com>
2023-02-15 15:27:46 +0200

    commit Homework2 John and Marta Task4

---
+++ Task3/John_amd_Marta/Task4.py
@@ -0,0 +1,10 @@
+"""
+4. Create boolean variables' john_gender and marta_gender.
+Let john be false and Marta be true. Print the values of both variables to the console using the print method.
+"""
+
+
+john_gender = False
+marta_gender = True
+
+print("John gender is: ", john_gender, "\nMarta gender is: ", marta_gender)
