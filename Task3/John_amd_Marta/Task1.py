c5f4ece974ccffb8b783d62c8520725319c4b7e8
med0k <zvoleikom@gmail.com>
2023-02-15 15:07:25 +0200

    commit Homework3 task4.7 and 4.8

---
+++ Task3/John_amd_Marta/Task1.py
@@ -0,0 +1,10 @@
+"""
+1. Create variables john_salary and marta_salary of some type (floating point).
+ Assign the created variables the values you want. Print the values of both variables to the console
+  using the print method.
+"""
+
+john_salary = 2555.5
+marta_salary = 3200.7
+
+print("John Salary is", john_salary, "\nMarta_salary is ", marta_salary)
