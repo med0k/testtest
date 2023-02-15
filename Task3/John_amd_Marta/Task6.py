123327cb988a3f1f56e285e8784a962ffedf4e05
med0k <zvoleikom@gmail.com>
2023-02-15 15:55:01 +0200

    commit Homework2 John and Marta Task6

---
+++ Task3/John_amd_Marta/Task6.py
@@ -0,0 +1,9 @@
+"""
+6. Create a list with people's names, some names should be repeated in it.
+Turn a list of duplicate names into a list of unique names using sets.
+"""
+
+list_friends = ['Petya', 'Misha', 'Lesha', 'Max', 'Gena', 'Gnom', 'Misha', 'Kurit', 'Lesha', 'Marijuana', 'Max']
+
+print(set(list_friends))
+
