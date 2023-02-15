74721e35db2bf278cf7aa3d3a968788b8146437d
med0k <zvoleikom@gmail.com>
2023-02-15 16:24:51 +0200

    commit Homework2 John and Marta Task7

---
+++ Task3/John_amd_Marta/Task7.py
@@ -0,0 +1,10 @@
+"""
+7. Create 2 tuple variables. The tuple must consist of 2 floating point numbers.
+The first value of the tuple is the latitude where you live, and the second value is the longitude where you live.
+"""
+
+
+tuple1 = (38.34049, -0.49957)
+
+print("Your position on map is: ", tuple1)
+print("Your position on map is: ", tuple1[0], "", tuple1[1])
