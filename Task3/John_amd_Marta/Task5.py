18fc72e193401ed725a500fa94f0e0b7870af604
med0k <zvoleikom@gmail.com>
2023-02-15 15:39:52 +0200

    commit Homework2 John and Marta Task5

---
+++ Task3/John_amd_Marta/Task5.py
@@ -0,0 +1,11 @@
+"""
+5. Create variables john_friends and marta_friends. Assign lists to variables.
+Each list must contain the names of friends. John has his list of friends and Martha has hers.
+Friends (friend's name) can be the usual strings "James", "Peter", etc.
+"""
+
+
+john_friends = ['Petya', 'Vasya', 'Nastya']
+marta_friends = ['Sasha', 'Lesha', 'Samka']
+
+print("Johnny's friends is: ", john_friends, "\nMarta's friends is: ", marta_friends)
