ccaa743f32dc96638cb666cb9bbcca8588eaf082
med0k <zvoleikom@gmail.com>
2023-02-15 15:09:36 +0200

    commit Homework3 task4.7 and 4.8

---
+++ Task3/John_amd_Marta/Task2.py
@@ -0,0 +1,10 @@
+"""
+2. Create variables john_age and marta_age of integer type.
+Assign the created variables the values you want. Print the values of both variables
+to the console using the print method.
+"""
+
+john_salary = 61500
+marta_salary = 128200
+
+print("John Salary is", john_salary, "\nMarta_salary is ", marta_salary)
